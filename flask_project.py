2526e44700fbd6c244eecd97d7996cc4e9127981
cheokjw <65707622+cheokjw@users.noreply.github.com>
2021-01-06 20:25:38 +0800

    changing files

--- flask_project.py
+++ flask_project.py
@@ -1,35 +1,30 @@
-from flask import Flask, redirect, url_for, render_template, request,session
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
+secretkey = "MdXzwwtT14Gh3"
 app = Flask(__name__)
-app.secret_key = "moonlight123"
+app.config["SECRET_KEY"] = secretkey
 
-@app.route("/") #home page
-def home():
+class UserForm(FlaskForm):
+    name = StringField("Username/ Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+@app.route("/")
+def index():
     return render_template("index.html")
 
-@app.route("/login", methods = ["POST", "GET"]) #GET = requesting information from user while POST = posting information into website
-def login():
-    if request.method == "POST": #direct user into other pages if input is successful.  
-        user = request.form["nm"]
-        session["user"] = user #giving session to the user
-        return redirect(url_for("user"))
-    else: 
-        if "user" in session:
-            return redirect(url_for("user"))
-        return render_template("login.html")
-    
-@app.route("/user")
-def user():
-    if "user" in session:
-        user = session["user"]
-        return render_template("index.html", usr = user)
-    else:
-        return redirect(url_for("login"))
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", name=name)
 
-@app.route("/logout")
-def logout():
-    session.pop("user", None)
-    return redirect(url_for("login"))
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+bootstrap = Bootstrap(app)
 
 if __name__ == "__main__":
     app.run(debug = True)
